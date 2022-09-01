7c1cd9eecf24fde778ff16506459d47bbfdc5c73
Ricardo Vicente <86174349+RicardoVicentepc@users.noreply.github.com>
2022-08-31 21:34:06 -0300

    Unico Update

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import {Text,View,TextInput,Button,StyleSheet, Image} from 'react-native';
+export default function Aplicacao(){
+  const [gasolina,setGasolina] = useState()
+  const [alcool,setAlcool] = useState()
+  const [resultado,setResultado] = useState()
+  const [imagem,setImagem] = useState()
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const imc = ()=>{
+    const r = parseFloat(alcool) / parseFloat(gasolina);
+    if(r < 0.7){
+      setResultado("Combustivel mais viavel é o Alcool")   
+      var img = require('./assets/images/download.jfif')
+      setImagem(<Image source={img} style={{width:200, height:200}}></Image>)
+
+    }else{
+      setResultado("Combustivel mais viavel é a Gasolina")
+      var img = require('./assets/images/gasosa.jfif')
+      setImagem(<Image source={img} style={{width:200, height:200}}></Image>)
+    }
+  }
+  return( 
+        <View style={styles.tudo}>
+          <View>
+            <Text style={styles.titulo}> Combustivel </Text>
+          </View>
+          <View style={{margin:15}}>
+            <Text>Alcool:  </Text>          
+            <TextInput              
+              style={styles.caixaDeTexto}
+              placeholder='Insira o alcool'
+              placeholderTextColor='#000'
+              autoFocus={true}
+              keyboardType={'numeric'}
+              onChangeText = {text =>setAlcool(text)}
+            />            
+          </View>   
+          <View style={{margin:15}}>
+            <Text>Gasolina:  </Text>          
+            <TextInput              
+              style={styles.caixaDeTexto}
+              placeholder='Insira a gasolina'
+              placeholderTextColor='#000'
+              autoFocus={true}
+              keyboardType={'numeric'}
+              onChangeText = {text =>setGasolina(text)}
+            />            
+          </View> 
+          <View style={{margin:15,}}>
+            <Button
+              title='Consultar '
+              //onPress={msg}
+              onPress = {()=>imc()}
+            />
+          </View>
+          <View style={{margin:15}}>
+              <Text style={styles.resposta}>{resultado}</Text>
+              <Text style={styles.resposta}>{imagem}</Text>
+          </View>
+          <View style={{margin:15}}>
+          </View>
+        </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  tudo:{
+    backgroundColor:'#fff',
+    height:'100%',
+    margin: 0,
+    padding:0,
   },
+  titulo:{
+    fontSize:30,
+    textAlign:'center',
+    marginTop:25,
+    marginBottom:10
+  },
+  caixaDeTexto:{
+    borderWidth:1,
+    borderRadius:40,
+    paddingLeft: 25,
+    height:60    
+  },
+  resposta:{
+    color:'#000',
+    fontSize:25,
+    textAlign:'center',
+    marginTop:30
+  }
 });
